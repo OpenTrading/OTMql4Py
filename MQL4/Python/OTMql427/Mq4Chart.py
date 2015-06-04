d97d429bca359953442143124314a3dc4043d3dc
Open Trading <OpenTrading@users.github.com>
2015-06-04 16:42:23 +0300

    Moved MQL4/Python/OTMql427/cmd2plus.py to OpenTrader added MQL4/Python/OTMql427/SimpleFormat.py

--- MQL4/Python/OTMql427/Mq4Chart.py
+++ MQL4/Python/OTMql427/Mq4Chart.py
@@ -7,6 +7,7 @@ A Mq4Chart object is a simple abstraction to encapsulate a Mt4 chart.
 import sys, logging
 import time
 import Queue
+from SimpleFormat import sFormatMessage
 
 oLOG = logging
 
@@ -63,38 +64,9 @@ class Mq4Chart(object):
         sTopic = 'retval'
         # FixMe: replace with sChartId
         sChartId = self.iChartId
-        sMess = self.sFormatMessage(sTopic, sChartId, sMark, sType, sValue)
+        sMess = sFormatMessage(sTopic, sChartId, sMark, sType, sValue)
         self.eMq4PushQueue(sMess)
     
-    def sFormatMessage(self, sMsgType, sChartId, sMark, sType, sValue):
-        # FixMe: the sMess must be in the right format
-        iIgnore = 0
-        if not sMark:
-            sMark = "%15.5f" % time.time()
-        sMess = "%s|%s|%d|%s|%s|%s" % (sMsgType, sChartId, iIgnore, sMark, sType, sValue,)
-        return sMess
-  
-    def lUnFormatMessage(self, sBody):
-        # FixMe:
-        """
-        The messaging to and from OTMql4Py is still being done with a
-        very simple format:
-              sMsgType|sChartId|sIgnored|sMark|sPayload
-        where sMsgType is one of: cmd eval (outgoing), timer tick retval (incoming);
-              sChartId is the Mt4 chart sChartId the message is to or from;
-              sMark is a simple floating point timestamp, with milliseconds;
-        and   sPayload is command|arg1|arg2... (outgoing) or type|value (incoming),
-              where type is one of: bool int double string json.
-        This breaks if the sPayload args or value contain a | 
-        We will probably replace this with json or pickled serialization, or kombu.
-        """
-        lArgs = sBody.split('|')
-        sCmd = lArgs[0]
-        sChart = lArgs[1]
-        sIgnore = lArgs[2]
-        sMark = lArgs[3]
-        return lArgs
-
     def vAdd(self, iId):
         self.iId = iId
         # see if it's already there
---
+++ MQL4/Python/OTMql427/SimpleFormat.py
@@ -0,0 +1,31 @@
+# -*-mode: python; py-indent-offset: 4; indent-tabs-mode: nil; encoding: utf-8-dos; coding: utf-8 -*-
+
+def sFormatMessage(sMsgType, sChartId, sMark, sType, sValue):
+    # FixMe: the sMess must be in the right format
+    iIgnore = 0
+    if not sMark:
+        sMark = "%15.5f" % time.time()
+    sMess = "%s|%s|%d|%s|%s|%s" % (sMsgType, sChartId, iIgnore, sMark, sType, sValue,)
+    return sMess
+
+def lUnFormatMessage(sBody):
+    # FixMe:
+    """
+    The messaging to and from OTMql4Py is still being done with a
+    very simple format:
+          sMsgType|sChartId|sIgnored|sMark|sPayload
+    where sMsgType is one of: cmd eval (outgoing), timer tick retval (incoming);
+          sChartId is the Mt4 chart sChartId the message is to or from;
+          sMark is a simple floating point timestamp, with milliseconds;
+    and   sPayload is command|arg1|arg2... (outgoing) or type|value (incoming),
+          where type is one of: bool int double string json.
+    This breaks if the sPayload args or value contain a | 
+    We will probably replace this with json or pickled serialization, or kombu.
+    """
+    lArgs = sBody.split('|')
+    sCmd = lArgs[0]
+    sChart = lArgs[1]
+    sIgnore = lArgs[2]
+    sMark = lArgs[3]
+    return lArgs
+
